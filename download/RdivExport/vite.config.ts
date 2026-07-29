e6c1af16f075f8e8be10108615d74bf88e49ecbe
Z User <z@container>
2026-07-29 15:55:13 +0000

    9379f831-359c-47e0-99f7-fa15aaad9d93

--- download/RdivExport/vite.config.ts
+++ download/RdivExport/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
